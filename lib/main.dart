c7980e027eaf6144b8d6f006fdb5e4802b19aae6
Jean Carlos <jean@naxostelecom.com.br>
2023-07-01 17:49:03 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Tarefas',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Tarefas'),
+          ),
+          body: Container(
+            color: Color.fromARGB(213, 207, 220, 236),
+            child: Stack(
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 140,
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text('Aprender Flutter'),
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up)),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print('clicou');
+            },
+            child: Text('+'),
+          ),
+        ));
+  }
+}
