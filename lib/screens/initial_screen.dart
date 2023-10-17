89be68a1de4bd1c0eef4cc430780fafc0c555029
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-10-17 00:07:53 -0300

    switch case listView

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/components/task.dart';
+import 'package:lista_de_tarefas/data/task_dao.dart';
 import 'package:lista_de_tarefas/data/task_inherited.dart';
 import 'package:lista_de_tarefas/screens/form_screen.dart';
 
@@ -18,14 +20,62 @@ class _InitialScreenState extends State<InitialScreen> {
         centerTitle: true,
         title: const Text('Tarefas'),
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.only(top: 8, bottom: 80),
-        children: TaskInherited.of(context)!.taskList,
+        child: FutureBuilder<List<Task>>(
+          future: TaskDao().findAll(),
+          builder: (context, snapshot) {
+            List<Task>? items = snapshot.data;
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                // TODO: Handle this case.
+                break;
+              case ConnectionState.waiting:
+                // TODO: Handle this case.
+                break;
+              case ConnectionState.active:
+                // TODO: Handle this case.
+                break;
+              case ConnectionState.done:
+                if (snapshot.hasData && items != null) {
+                  if (items.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final Task tarefa = items[index];
+                        return tarefa;
+                      },
+                    );
+                  }
+                  return const Center(
+                    child: Column(
+                      children: [
+                        Icon(Icons.error_outline),
+                        Text(
+                          'Não há nenhuma Tarefa!!!',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                // return const Text('Erro ao carregar tarefas');
+                break;
+            }
+            return const Text('Erro desconhecido!!!');
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context, MaterialPageRoute(builder: (contextNew) => FormScreen(taskContext: context,)));
+            context,
+            MaterialPageRoute(
+              builder: (contextNew) => FormScreen(
+                taskContext: context,
+              ),
+            ),
+          );
         },
         child: const Icon(Icons.add),
       ),
